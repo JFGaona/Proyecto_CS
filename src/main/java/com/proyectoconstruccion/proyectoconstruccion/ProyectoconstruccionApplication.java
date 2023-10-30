bb47de1fbd121b21555b8d85b354769df6edb58f
Jonathan <Jonathan@192.168.56.1>
2023-10-30 11:14:45 -0500

    Avance del proyecto

---
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/ProyectoconstruccionApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoconstruccion.proyectoconstruccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoconstruccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoconstruccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoconstruccion/proyectoconstruccion/ProyectoconstruccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoconstruccion.proyectoconstruccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoconstruccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
