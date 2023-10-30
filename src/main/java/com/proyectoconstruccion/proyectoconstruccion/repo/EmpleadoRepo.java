c6b4afc8bb0618fac921c5f280584f38fefbc65a
Jonathan <Jonathan@192.168.56.1>
2023-10-30 14:54:09 -0500

    Funcionalidades del CRUD implementadas, crear,act y listar

--- src/main/java/com/proyectoconstruccion/proyectoconstruccion/repo/EmpleadoRepo.java
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/repo/EmpleadoRepo.java
@@ -3,11 +3,13 @@ package com.proyectoconstruccion.proyectoconstruccion.repo;
 
 import com.proyectoconstruccion.proyectoconstruccion.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Jonathan
  */
+@Repository
 public interface EmpleadoRepo extends JpaRepository<Empleado, Integer>{
     
 }
--- src/main/java/com/proyectoconstruccion/proyectoconstruccion/service/EmpleadoService.java
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/service/EmpleadoService.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.proyectoconstruccion.proyectoconstruccion.service;
+
+import com.proyectoconstruccion.proyectoconstruccion.entity.Empleado;
+import com.proyectoconstruccion.proyectoconstruccion.repo.EmpleadoRepo;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Jonathan
  */
-public class EmpleadoService {
+
+@Service
+public class EmpleadoService{
+    
+    @Autowired
+    private EmpleadoRepo empleadorepo;
+    
+    public Empleado insertar(Empleado emp){
+        return empleadorepo.save(emp);
+    }
+    
+    public Empleado actualizar(Empleado emp){
+        return empleadorepo.save(emp);
+    }
+    
+    public List<Empleado> listar(){
+        return empleadorepo.findAll();
+    }
     
 }
