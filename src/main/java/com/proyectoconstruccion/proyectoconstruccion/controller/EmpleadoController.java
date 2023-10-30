f12b9715512aeeba0e5811abea866f8177397fa5
Jonathan <Jonathan@192.168.56.1>
2023-10-30 15:05:54 -0500

    GetMapping POST Y PUT hechos

--- src/main/java/com/proyectoconstruccion/proyectoconstruccion/controller/EmpleadoController.java
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/controller/EmpleadoController.java
@@ -6,6 +6,9 @@ import com.proyectoconstruccion.proyectoconstruccion.service.EmpleadoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +28,17 @@ public class EmpleadoController {
     public List<Empleado> listar(){
         return empleadoservice.listar();  
     }
+    
+    @PostMapping
+    public Empleado insertar(@RequestBody Empleado emp){
+        return empleadoservice.insertar(emp);
+        
+    }
+    
+    @PutMapping
+    public Empleado actualizar(@RequestBody Empleado emp){
+        return empleadoservice.actualizar(emp);
+    }
+    
+    
 }
