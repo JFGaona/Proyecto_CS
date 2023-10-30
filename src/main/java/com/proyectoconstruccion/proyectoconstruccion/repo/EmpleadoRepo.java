46348b26c2297e0672151b71df19ab871e3adccf
Jonathan <Jonathan@192.168.56.1>
2023-10-30 14:46:13 -0500

    Empleado Repo creado e importado correctamente

---
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/repo/EmpleadoRepo.java
@@ -0,0 +1,15 @@
+
+package com.proyectoconstruccion.proyectoconstruccion.repo;
+
+import com.proyectoconstruccion.proyectoconstruccion.entity.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Jonathan
+ */
+public interface EmpleadoRepo extends JpaRepository<Empleado, Integer>{
+    
+    
+    
+}
