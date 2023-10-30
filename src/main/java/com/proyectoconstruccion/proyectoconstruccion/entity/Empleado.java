e154b4a9afdc3bf70862a72898850a21434a39fd
Jonathan <Jonathan@192.168.56.1>
2023-10-30 12:00:04 -0500

    Getter and Setter de Empleado generados

--- src/main/java/com/proyectoconstruccion/proyectoconstruccion/entity/Empleado.java
+++ src/main/java/com/proyectoconstruccion/proyectoconstruccion/entity/Empleado.java
@@ -28,5 +28,63 @@ public class Empleado {
     private Integer edad;
     private String telefono;
     private Date fecha_nacimiento;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    
     
 }
