fa83bb2b836e6d22129f1d049393cd225de1e64d
Oscar Felipe Rodriguez Piñeros <ofrodriguez42@ucatolica.edu.co>
2024-05-06 18:09:59 -0500

    Solution problem with POM a Imports
    Solution to execute, and dependencies and imports in class UserModel

--- src/main/java/com/csw/proyecto/demo/DemoApplication.java
+++ src/main/java/com/csw/proyecto/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.csw.proyecto.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan(basePackages = {"com.csw.proyecto.demo"})
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/csw/proyecto/demo/Repositories/IUserRepository.java
+++ src/main/java/com/csw/proyecto/demo/Repositories/IUserRepository.java
@@ -1,10 +1,8 @@
 package com.csw.proyecto.demo.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import com.csw.proyecto.demo.Models.UserModel;
 import org.springframework.stereotype.Repository;
+import com.csw.proyecto.demo.Models.UserModel;
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
