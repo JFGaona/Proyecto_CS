a626998844fefa77bf9209a225f6f3f451cdc904
Lady726 <ljbarrios52@ucatolica.edu.co>
2024-05-29 12:56:49 -0500

    usuario

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.demo.model.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Método para buscar un usuario por su nombre
+  User findByName(String name);
+  // Método para buscar un usuario por su correo electrónico
+  User findByEmail(String email);
+  // Método para verificar si existe un usuario con un correo electrónico específico
+  boolean existsByEmail(String email);
+}
